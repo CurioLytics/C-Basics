59d8f7b85cb12efd009f9e23426c9d23737f7ccd
Nguyen Ly <122440395+CurioLytics@users.noreply.github.com>
2024-09-26 16:03:33 +0700

    update main file for csharp syntax

--- Program.cs
+++ Program.cs
@@ -1,22 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace MyFirstProject
+namespace HelloWorld
 {
-    internal static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
+
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.Write("how many times? ");
+            int loopTime = Convert.ToInt32(Console.ReadLine());
+
+            while (loopTime < 0) 
+            {
+            Console.WriteLine("enter value > 0 ");
+            Console.Write("how many times? ");
+            loopTime = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < loopTime; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+            
+
         }
     }
 }
